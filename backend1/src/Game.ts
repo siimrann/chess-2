c39c102a766592f6434d5c0159f75797a14ef8a1
SImran Tyagi <111629975+siimrann@users.noreply.github.com>
2024-06-20 14:41:37 +0530

    complete

---
+++ backend1/src/Game.ts
@@ -0,0 +1,87 @@
+import { Chess } from "chess.js";
+import { WebSocket } from "ws";
+import { GAME_OVER, INIT_GAME, MOVE } from "./messages";
+
+export class Game {
+  public player1: WebSocket;
+  public player2: WebSocket;
+  private board: Chess;
+  private startTime: Date;
+  private moveCount = 0;
+
+  constructor(player1: WebSocket, player2: WebSocket) {
+    this.player1 = player1;
+    this.player2 = player2;
+    this.board = new Chess();
+    this.startTime = new Date();
+
+    this.player1.send(
+      JSON.stringify({
+        type: INIT_GAME,
+        payload: {
+          color: "white",
+        },
+      })
+    );
+    this.player2.send(
+      JSON.stringify({
+        type: INIT_GAME,
+        payload: {
+          color: "black",
+        },
+      })
+    );
+  }
+
+  makeMove(
+    socket: WebSocket,
+    move: {
+      from: string;
+      to: string;
+    }
+  ) {
+    if (this.moveCount % 2 === 0 && socket !== this.player1) {
+      return;
+    }
+    if (this.moveCount % 2 === 0 && socket !== this.player1) {
+      return;
+    }
+
+    try {
+      this.board.move(move);
+      // this.moveCount++;
+    } catch (err) {
+      console.log(err);
+    }
+
+    if (this.board.isGameOver()) {
+      this.player1.emit(
+        JSON.stringify({
+          type: GAME_OVER,
+          payload: {
+            winner: this.board.turn() === "w" ? "black" : "white",
+          },
+        })
+      );
+      return;
+    }
+
+    if (this.board.moves().length % 2 === 0) {
+      this.player2.send(
+        JSON.stringify({
+          type: MOVE,
+          payload: move,
+        })
+      );
+    } else {
+      this.player1.send(
+        JSON.stringify({
+          type: MOVE,
+          payload: move,
+        })
+      );
+    }
+
+    this.moveCount++;
+  }
+}
